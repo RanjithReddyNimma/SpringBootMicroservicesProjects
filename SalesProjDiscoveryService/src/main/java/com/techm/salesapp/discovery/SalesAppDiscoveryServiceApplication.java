551b1cba71ab6f6fad9650ffa1de7b50d55d58a1
NR00577817 <NR00577817@INHYIZPC12200.TechMahindra.com>
2019-08-07 12:58:17 +0530

    First Commit

---
+++ SalesProjDiscoveryService/src/main/java/com/techm/salesapp/discovery/SalesAppDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.techm.salesapp.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SalesAppDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesAppDiscoveryServiceApplication.class, args);
+	}
+
+}
